24ce4d206f767f58966307ea7aa4fcf8e401935c
Ayala-Yagelnik <a83245064@gmail.com>
2025-02-28 02:13:45 +0200

    initial commit

---
+++ src/IntelliDocs.Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using IntelliDocs.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliDocs.Data
+{
+    class DataContext
+    {
+        public List<User> Users { get; set; }
+    }
+}
---
+++ src/IntelliDocs.Service/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace IntelliDocs.Service
+{
+    public class Class1
+    {
+
+    }
+}
